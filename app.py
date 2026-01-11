ccadbb47a22e7b36c3c93d9771bc253786b104e2
Felipe Ferras <frfjugler@gmail.com>
2026-01-10 20:27:13 -0600

    3ra version (ruta absoluta)

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv(
-    "C:\\Users\\Felipe Rodriguez\\Desktop\\TripleTen\\p7\\vehicles_us.csv")
+car_data = pd.read_csv("vehicles_us.csv")
 
 st.header("Información Automotriz")
 
